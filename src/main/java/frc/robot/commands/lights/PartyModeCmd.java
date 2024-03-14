2be64877b72b819aad860b112fd9fc4a8c3a2c09
LoganKippnick <lgkippnick@outlook.com>
2024-03-13 23:47:33 -0400

    uses translate methods

--- src/main/java/frc/robot/commands/lights/PartyModeCmd.java
+++ src/main/java/frc/robot/commands/lights/PartyModeCmd.java
@@ -4,12 +4,12 @@
 
 package frc.robot.commands.lights;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.LightsConstants;
 import frc.robot.subsystems.LightsSys;
 import frc.robot.util.led.LEDStrip;
+import frc.robot.util.led.LEDStrip.TranslateDirection;
 
 public class PartyModeCmd extends Command {
 
@@ -26,12 +26,9 @@ public class PartyModeCmd extends Command {
 
 	// Called when the command is initially scheduled.
 	@Override
-	public void initialize() {}
-
-	// Called every time the scheduler runs while the command is scheduled.
-	@Override
-	public void execute() {
+	public void initialize() {
 		for(LEDStrip strip : new LEDStrip[] {lightsSys.exampleStrip1, lightsSys.exampleStrip2, lightsSys.exampleStrip3}) {
+			strip.setValue(1.0);
 			int hue = startingHue;
 			for(int i = 0; i < strip.getLength(); i++) {
 				strip.setColor(Color.fromHSV(hue, 255, 255), i);
@@ -43,7 +40,21 @@ public class PartyModeCmd extends Command {
 				}
 			}
 		}
-		startingHue += LightsConstants.partyModeHueIncrement;
+	}
+
+	// Called every time the scheduler runs while the command is scheduled.
+	@Override
+	public void execute() {
+		if(startingHue >= 255) {
+			startingHue = 0;
+		}
+		else {
+			startingHue += LightsConstants.partyModeHueIncrement;
+		}
+		
+		for(LEDStrip strip : new LEDStrip[] {lightsSys.exampleStrip1, lightsSys.exampleStrip2, lightsSys.exampleStrip3}) {
+			strip.translateColors(TranslateDirection.FORWARD, Color.fromHSV(startingHue, 255, 255));
+		}
 	}
 
 	// Called once the command ends or is interrupted.
